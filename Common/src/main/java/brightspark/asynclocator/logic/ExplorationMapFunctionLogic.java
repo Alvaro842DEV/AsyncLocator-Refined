f0c6e1a157ba0b679780de9e86deae999ff0cb0f
Alvaro842DEV <139498211+Alvaro842DEV@users.noreply.github.com>
2025-08-17 23:22:18 +0200

    i forgot to delete it

--- Common/src/main/java/brightspark/asynclocator/logic/ExplorationMapFunctionLogic.java
+++ Common/src/main/java/brightspark/asynclocator/logic/ExplorationMapFunctionLogic.java
@@ -2,7 +2,6 @@ package brightspark.asynclocator.logic;
 
 import brightspark.asynclocator.ALConstants;
 import brightspark.asynclocator.AsyncLocator;
-import brightspark.asynclocator.AsyncMapTracker;
 import brightspark.asynclocator.platform.Services;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
@@ -49,70 +48,4 @@ public class ExplorationMapFunctionLogic {
         }
         return name;
     }
-
-	private static void processLocationResult(UUID trackingId, BlockPos foundLocation) {
-		AsyncMapTracker.getInstance().completeLocationOperation(trackingId, foundLocation);
-	}
-
-	public static void handleLocationFound(
-		ItemStack mapStack,
-		ServerLevel level,
-		BlockPos pos,
-		int scale,
-		Holder<MapDecorationType> destinationTypeHolder,
-		BlockPos invPos
-	) {
-		if (pos == null) {
-			ALConstants.logInfo("No location found - invalidating map stack in inventory at {}", invPos);
-			Services.EXPLORATION_MAP_FUNCTION_LOGIC.invalidateMap(mapStack, level, invPos);
-		} else {
-			ALConstants.logInfo("Location found at {} - updating treasure map in inventory at {}", pos, invPos);
-			Component mapName = getCachedName(mapStack);
-			Services.EXPLORATION_MAP_FUNCTION_LOGIC.updateMap(
-				mapStack,
-				level,
-				pos,
-				scale,
-				destinationTypeHolder,
-				invPos,
-				mapName
-			);
-		}
-	}
-
-	public static ItemStack updateMapAsync(
-		ServerLevel level,
-		BlockPos blockPos,
-		int scale,
-		int searchRadius,
-		boolean skipKnownStructures,
-		Holder<MapDecorationType> destinationTypeHolder,
-		TagKey<Structure> destination
-	) {
-		ItemStack mapStack = CommonLogic.createManagedMap();
-		ALConstants.logDebug("Created managed map stack for async update (Search origin: {})", blockPos);
-
-		// Extract UUID from the managed map for tracking
-		var customData = mapStack.get(net.minecraft.core.component.DataComponents.CUSTOM_DATA);
-		if (customData != null) {
-			UUID operationId = customData.copyTag().getUUID("asynclocator.pending.uuid");
-			
-			boolean isChestGenerated = (level.getBlockEntity(blockPos) instanceof net.minecraft.world.level.block.entity.ChestBlockEntity);
-			
-			AsyncMapTracker.getInstance().addLocationOperation(operationId, 
-				new AsyncMapTracker.MapLocationOperation(level.dimension(), scale, destinationTypeHolder));
-
-			// Start the async location process
-			AsyncLocator.locate(level, destination, blockPos, searchRadius, skipKnownStructures)
-				.thenOnServerThread(foundPos -> {
-					if (isChestGenerated) {
-						handleLocationFound(mapStack, level, foundPos, scale, destinationTypeHolder, blockPos);
-					} else {
-						processLocationResult(operationId, foundPos);
-					}
-				});
-		}
-		
-		return mapStack;
-	}
 }
