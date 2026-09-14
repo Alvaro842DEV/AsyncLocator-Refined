46870abec3b644ca1f821ca9bc72ab3416192104
Alvaro842DEV <139498211+Alvaro842DEV@users.noreply.github.com>
2026-09-14 18:54:51 +0200

    fix(maps): prevent pickup of all pending maps

--- Common/src/main/java/brightspark/asynclocator/mixins/SlotMixin.java
+++ Common/src/main/java/brightspark/asynclocator/mixins/SlotMixin.java
@@ -2,7 +2,6 @@ package brightspark.asynclocator.mixins;
 
 import brightspark.asynclocator.ALConstants;
 import brightspark.asynclocator.logic.CommonLogic;
-import brightspark.asynclocator.platform.Services;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.world.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,12 +11,10 @@ import org.spongepowered.asm.mixin.injection.At;
 public class SlotMixin {
     @ModifyReturnValue(method = "mayPickup", at = @At("RETURN"))
     private boolean preventPickupOfPendingExplorationMap(boolean original) {
-        if (Services.CONFIG.explorationMapEnabled()) {
-            Slot slot = (Slot) (Object) this;
-            if (CommonLogic.isEmptyPendingMap(slot.getItem())) {
-                ALConstants.logDebug("Intercepted Slot#mayPickup call");
-                return false;
-            }
+        Slot slot = (Slot) (Object) this;
+        if (CommonLogic.isEmptyPendingMap(slot.getItem())) {
+            ALConstants.logDebug("Intercepted Slot#mayPickup call");
+            return false;
         }
         return original;
     }
